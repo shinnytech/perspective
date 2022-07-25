d4d435fa8cebe601e96b72fb7d86f4fe548f2916
shinny-tanyan <tanyan@shinnytech.com>
2022-07-25 09:39:00 +0800

    add: add compile files

---
+++ static/perspective-viewer/dist/esm/init.d.ts
@@ -0,0 +1,10 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2018, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms
+ * of the Apache License 2.0.  The full license can be found in the LICENSE
+ * file.
+ *
+ */
+export declare const WASM_MODULE: Promise<import("@finos/perspective-viewer/dist/pkg/perspective_viewer.js").InitOutput>;
---
+++ static/perspective-viewer/dist/pkg/snippets/perspective-viewer-362cf5cec2bfcc07/inline1.js
@@ -0,0 +1,5 @@
+
+    import * as monaco from 'monaco-editor/esm/vs/editor/editor.worker.js';
+    export default async function () {
+        return await monaco.initialize();
+    }
